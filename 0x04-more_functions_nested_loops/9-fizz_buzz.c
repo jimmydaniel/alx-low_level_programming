05a7cb1799037f27117d1ec0e8dec42fe036fc47
jimmydaniel <mamiye05@gmail.com>
2022-10-20 18:42:54 -0700

    quiz fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,34 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - print fizzbuzz
- * Return: Always 0
+ * main - prints Buzz for multiples of 5.
+ * Fizz for multiples of 3
+ * FizzBuzz for multiples of both
+ * Return: 0
  */
 int main(void)
 {
 	int n;
 
-	printf("1");
+	n = 1;
+	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
-		printf(" ");
-		if (n % 15 == 0)
-			printf("FizzBuzz");
-		if (n % 3 == 0)
-			printf("Fizz");
-		if (n % 5 == 0)
-			printf("Buzz");
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
 	}
 	printf("\n");
 	return (0);
