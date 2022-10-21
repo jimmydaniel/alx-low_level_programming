41eba6730c4f4400bb87e144b5be5502ba9e2211
jimmydaniel <mamiye05@gmail.com>
2022-10-20 18:36:39 -0700

    FizzBuzz numbers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,8 +16,8 @@ int main(void)
 			printf("Fizz");
 		if (n % 5 == 0)
 			printf("Buzz");
-		if (n % 3 != 0 && n % 5 != 0)
-			printf("%d", n);
+		if (n % 15 == 0)
+			printf("FizzBuzz");
 	}
 	printf("\n");
 	return (0);
