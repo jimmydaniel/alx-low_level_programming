df53e812fb6f00507fdfbc7a6a395fa57df04a79
jimmydaniel <mamiye05@gmail.com>
2022-11-23 22:34:35 -0800

    64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+; Filename: holberton.asm
+; Author: Danny Mulugeta
+
+global main
+extern printf
+
+section .text
+
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
